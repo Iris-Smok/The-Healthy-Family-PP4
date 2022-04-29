cfb50d141607cc24e1d7847e10ba50e0b1e77711
Iris-Smok <irissmok11@gmail.com>
2022-04-29 09:51:33 +0000

    Installed Summernote, add it to Post class

--- recipes/admin.py
+++ recipes/admin.py
@@ -2,7 +2,13 @@
 admin.py
 """
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Comment
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description', 'ingredients', ' preparation_steps')
+
+
 admin.site.register(Comment)
