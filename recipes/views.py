c6c118315dad1921abeb383f2d69162fd6f3a159
Iris-Smok <irissmok11@gmail.com>
2022-04-29 11:48:36 +0000

    Add view for home page

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,13 @@
+"""
+Views.py
+"""
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Post
 
-# Create your views here.
+
+class PostList(TemplateView):
+    """
+    Home page view
+    """
+    template_name = 'index.html'
