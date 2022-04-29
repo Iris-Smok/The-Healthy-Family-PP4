7ba1d075bfb26af5d3a15be487149108e96fb3a7
Iris-Smok <irissmok11@gmail.com>
2022-04-29 09:18:59 +0000

    Create Post model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,47 @@
+"""
+models.py
+"""
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    Post model
+    """
+    title = models.CharField(max_length=200, unique=True, blank=False)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='blog_posts')  # noqa
+    published_on = models.DateField(auto_now_add=True)
+    description = models.TextField()
+    ingredients = models.TextField()
+    preparation_steps = models.TextField()
+    image = CloudinaryField('image', default='placeholder')
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+    comments = models.ManyToManyField(User, related_name='blog_comments', blank= True)  # noqa
+
+    class Meta:
+        """
+        ordering
+        """
+        ordering = ['-published_on']
+
+    def __str__(self):
+        """
+        string title
+        """
+        return self.title
+
+    def number_of_likes(self):
+        """
+        return likes count
+        """
+        return self.likes.count()
+
+    def number_of_comments(self):
+        """
+        return comment count
+        """
+        return self.comments.count()
+
