e2784357621068e97dc1fda98d21291ebd8f777f
Iris-Smok <irissmok11@gmail.com>
2022-05-13 16:09:03 +0000

    Create HomePage view to render last added recipe

--- recipes/views.py
+++ recipes/views.py
@@ -3,30 +3,44 @@ Views.py
 """
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views import View
+from django.db.models import Count
 from .models import Post
 
 
-class PostList(TemplateView):
+class HomePage(View):
     """
     Home page view
+    view for last added recipes and most loved recipes
     """
-    template_name = 'index.html'
+
+    def get(self, request):
+        """
+        get request
+        """
+        posts = Post.objects.order_by('-published_on')[:4]
+        context = {
+            "posts": posts,
+        }
+        return render(request, 'index.html', context)
+
 
 class AllRecipes(TemplateView):
     """
     all_recipes view
     """
-    template_name='all_recipes.html'
+    template_name = 'all_recipes.html'
 
 
 class LogIn(TemplateView):
     """
     all_recipes view
     """
-    template_name='login.html'
+    template_name = 'login.html'
+
 
 class Register(TemplateView):
     """
     all_recipes view
     """
-    template_name='register.html'
+    template_name = 'register.html'
