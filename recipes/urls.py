2e9da3b214b4f01e670c2459ecd7ab22df5166a3
Iris-Smok <irissmok11@gmail.com>
2022-05-13 16:09:44 +0000

    Add path for HopePage view

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,7 +6,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path('', views.HomePage.as_view(), name='home'),
     path('all_recipes', views.AllRecipes.as_view(), name='all_recipes'),
     path('login', views.LogIn.as_view(), name='login'),
     path('register', views.Register.as_view(), name='register'),
