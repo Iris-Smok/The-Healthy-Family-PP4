ae66265ad0f04dc9e94fbced03b0bb8d8c417601
Iris-Smok <irissmok11@gmail.com>
2022-05-13 16:07:02 +0000

    Add DateTimeField for published_on field

--- recipes/models.py
+++ recipes/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True, blank=False)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='blog_posts')  # noqa
-    published_on = models.DateField(auto_now_add=True)
+    published_on = models.DateTimeField(auto_now_add=True)
     description = models.TextField()
     ingredients = models.TextField()
     preparation_steps = models.TextField()
