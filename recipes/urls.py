19419cfbd3663cd70eba5cd29660d46b72602ede
Iris-Smok <irissmok11@gmail.com>
2022-05-17 14:11:27 +0000

    Create url for recipe details page

--- recipes/urls.py
+++ recipes/urls.py
@@ -8,6 +8,5 @@ from . import views
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
     path('all_recipes', views.AllRecipes.as_view(), name='all_recipes'),
-    path('login', views.LogIn.as_view(), name='login'),
-    path('register', views.Register.as_view(), name='register'),
+    path('<slug:slug>/', views.RecipeDetails.as_view(), name="recipe_details"),
 ]
