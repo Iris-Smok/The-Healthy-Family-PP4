f8cc4c99ff39c03bd5849de0a1a7b48699c3d56e
Iris-Smok <irissmok11@gmail.com>
2022-05-20 11:54:30 +0000

    Create Comment form

---
+++ recipes/forms.py
@@ -0,0 +1,14 @@
+from django import forms 
+from .models import Comment
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.fields[
+            'body'
+            ].label = ""
