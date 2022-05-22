9852b5d88c6fb9508121f3fd3904df6f87ad8b05
Iris-Smok <irissmok11@gmail.com>
2022-05-22 13:20:31 +0000

    Add width,height and Toolbar customization,show summernote with Bootstrap4 for Summernote fields in add recipe page

--- healthy_family/settings.py
+++ healthy_family/settings.py
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
 
 CRISPY_TEMPLATES_PACK = 'bootstrap4'
 
+SUMMERNOTE_THEME = 'bs4'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -146,3 +148,14 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'  # no
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SUMMERNOTE_CONFIG = {
+    # Change editor size
+    # instructions taken from https://github.com/summernote/django-summernote
+    'width': '100%',
+    'height': '480',
+    'toolbar': [
+            ['font', ['bold', 'underline', 'italic', 'clear']],
+            ['para', ['ul', 'ol']],
+        ],
+}
