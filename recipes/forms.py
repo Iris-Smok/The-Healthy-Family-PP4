02477bbee499f159e5a24c97faaad1c5d1cade3b
Iris-Smok <irissmok11@gmail.com>
2022-05-22 10:24:22 +0000

    Create form for add recipe, add docstrings and remove whitespace

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,9 +1,13 @@
-from django import forms 
-from .models import Comment
+""" forms for Comment and Recipe """
+from django import forms
+from django_summernote.widgets import SummernoteWidget
+from .models import Comment, Post
 
 
 class CommentForm(forms.ModelForm):
+    """ Comment Form"""
     class Meta:
+        """ fields for comment form"""
         model = Comment
         fields = ('body',)
 
@@ -12,3 +16,23 @@ class CommentForm(forms.ModelForm):
         self.fields[
             'body'
             ].label = ""
+
+
+class RecipeForm(forms.ModelForm):
+    """ Recipe Form """
+    class Meta:
+        """ fields for recipe form"""
+        model = Post
+        fields = ('title', 'description', 'ingredients',
+                  'preparation_steps', 'image')
+
+        widgets = {
+            'ingredients': SummernoteWidget(),
+            'preparation_steps': SummernoteWidget(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(RecipeForm, self).__init__(*args, **kwargs)
+        self.fields[
+            'image'
+            ].label = "You can upload an image here"
