7f783d33bc3e5cf2d48d03b4b4906e81e743f282
Iris-Smok <irissmok11@gmail.com>
2022-05-23 12:24:03 +0000

    Add allautsocial logins to installed apps, add site redirect when login/logout and message tags

--- healthy_family/settings.py
+++ healthy_family/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env  # noqa
 
@@ -42,12 +43,29 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'cloudinary_storage',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
     'crispy_forms',
     'recipes',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-denger',
+}
 
 CRISPY_TEMPLATES_PACK = 'bootstrap4'
 
