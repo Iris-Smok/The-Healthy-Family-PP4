c3d8f5ab056a9d0e487b5f27c4f4816095d1cdda
Iris-Smok <irissmok11@gmail.com>
2022-05-23 12:24:57 +0000

    Add url path for allauth urls

--- healthy_family/urls.py
+++ healthy_family/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('recipes.urls'), name='recipes_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
