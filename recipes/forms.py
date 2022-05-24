c62fa24597ac6fdc17072ee36c5305de910f1c04
Iris-Smok <irissmok11@gmail.com>
2022-05-24 23:17:37 +0000

    Create contact form

--- recipes/forms.py
+++ recipes/forms.py
@@ -36,3 +36,11 @@ class RecipeForm(forms.ModelForm):
         self.fields[
             'image'
             ].label = "You can upload an image here"
+
+
+class ContactForm(forms.Form):
+    """ Contact form """
+    first_name = forms.CharField(label='Your name', max_length=100)
+    last_name = forms.CharField(max_length=50)
+    email_address = forms.EmailField(max_length=150)
+    message = forms.CharField(widget=forms.Textarea, max_length=2000)
