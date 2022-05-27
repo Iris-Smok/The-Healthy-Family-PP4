d9bac8fd0d2b9be74b01495a997770b04291e5c7
Iris-Smok <irissmok11@gmail.com>
2022-05-27 21:30:24 +0000

    Add load static on each page

--- recipes/forms.py
+++ recipes/forms.py
@@ -41,6 +41,5 @@ class RecipeForm(forms.ModelForm):
 class ContactForm(forms.Form):
     """ Contact form """
     first_name = forms.CharField(label='Your name', max_length=100)
-    last_name = forms.CharField(max_length=50)
-    email_address = forms.EmailField(max_length=150)
+    email_address = forms.EmailField(max_length=150, label='Your Email')
     message = forms.CharField(widget=forms.Textarea, max_length=2000)
