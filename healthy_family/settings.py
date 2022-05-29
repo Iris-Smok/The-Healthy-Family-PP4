dd9a1ddc7552018b73d3ea0ffac701d4a7ef03ea
Iris-Smok <irissmok11@gmail.com>
2022-05-29 23:34:49 +0000

    Set database for local and heroku development

--- healthy_family/settings.py
+++ healthy_family/settings.py
@@ -114,17 +114,19 @@ WSGI_APPLICATION = 'healthy_family.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+development = os.environ.get('DEVELOPMENT', False)
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
-
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
